a382616e3bd11d5a4dbbf524c2b56e449959ca00
takeshix <konno@coosy.co.jp>
2014-02-04 17:33:18 +0900

    add partial method

--- View/TwigView.php
+++ View/TwigView.php
@@ -11,9 +11,11 @@
  * @author Kjell Bublitz <m3nt0r.de@gmail.com>
  * @link http://github.com/m3nt0r My GitHub
  * @link http://twitter.com/m3nt0r My Twitter
+ * @link https://github.com/kozo/Partial
  * @author Graham Weldon (http://grahamweldon.com)
  * @license The MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+App::uses('Folder', 'Utility');
 if (!defined('TWIG_VIEW_CACHE')) {
 	define('TWIG_VIEW_CACHE', CakePlugin::path('TwigView') . 'tmp' . DS . 'views');
 }
@@ -72,6 +74,7 @@ class TwigView extends View {
  */
 	public $templatePaths = array();
 
+	public $partialCache = 'partial';
 /**
  * Constructor
  * Overridden to provide Twig loading
@@ -182,5 +185,88 @@ class TwigView extends View {
 		$return = parent::element($name, $params, $callbacks);
 		//$this->ext = '.tpl';
 		return $return;
+	}
+
+	function partial($name, $data = array(), $options = array(), $loadHelpers = true) {
+		$file = $plugin = $key = null;
+
+		// キャッシュの設定(フォルダがない場合は新規作成)
+		$cachePath = TMP . 'cache' . DS . 'partial' . DS;
+		$obj = new Folder($cachePath, true, 0777);
+		Cache::config($this->partialCache, array('engine'=>'File', 'path' => $cachePath));
+
+		$plugin = $this->plugin;
+		if (isset($options['plugin'])) {
+			$plugin = Inflector::camelize($options['plugin']);
+		}
+
+		if (isset($options['cache'])) {
+			$underscored = null;
+			if ($plugin) {
+				$underscored = Inflector::underscore($plugin);
+			}
+			$keys = array_merge(array($this->viewPath, $this->action, $underscored, $name), array_keys($options), array_keys($data));
+			$caching = array(
+				'config' => $this->partialCache,
+				'key' => implode('_', $keys)
+			);
+			if (is_array($options['cache'])) {
+				$defaults = array(
+					'config' => $this->partialCache,
+					'key' => $caching['key']
+				);
+				$caching = array_merge($defaults, $options['cache']);
+			}
+			$key = 'partial_' . $caching['key'];
+			$contents = Cache::read($key, $caching['config']);
+			if ($contents !== false) {
+				return $contents;
+			}
+		}
+
+		// 「_」をつける
+		$buf = explode(DS, $name);
+		$buf[count($buf)-1] = '_' . $buf[count($buf)-1];
+		$name = implode(DS, $buf);
+
+		// ファイルパス取得
+		$fullPath = $this->_getPartialFileName($name, $plugin);
+		if ($fullPath !== false) {
+			if ($loadHelpers === true) {
+				$this->loadHelpers();
+			}
+
+			$partial = $this->_render($fullPath, array_merge($this->viewVars, $data));
+
+			if (isset($options['cache'])) {
+				Cache::write($key, $partial, $caching['config']);
+			}
+			return $partial;
+		}
+		$file = $this->viewPath . DS . $name . $this->ext;
+
+		if (Configure::read('debug') > 0) {
+			return "Partial Not Found: " . $file;
+		}
+	}
+
+	/**
+	 * ファイル名を取得する
+	 *
+	 * @access private
+	 * @author sakuragawa
+	 */
+	private function _getPartialFileName($name, $plugin) {
+		$paths = App::path('View', $plugin);
+		$exts = $this->_getExtensions();
+
+		foreach ($exts as $ext) {
+			foreach ($paths as $path) {
+				if (file_exists($path . $this->viewPath . DS . $name . $ext)) {
+					return $path . $this->viewPath . DS . $name . $ext;
+				}
+			}
+		}
+		return false;
 	}
 }
